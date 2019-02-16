0802fe767181cb9a56c5058a45eea73de48e5e17
rlama531 <roberto.lama18@gmail.com>
2019-02-16 01:44:57 -0800

    Added The get_rep_access()

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template, url_for, request
 import json
 import os
 import random
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
     return render_template('index.html')
+
+@app.route('/access_rep_info', methods=['GET'])
+def get_rep_access():
+    url = "https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators"
+    content = request.get_json()
+
+    querystring = {"address": content['zipcode']}
+
+    headers = {
+        'x-api-key': "2e1uvo7yeX50ZGHvctPxi8ZWubhggyOydIWvOa5c",
+        'Content-Type': "application/x-www-form-urlencoded",
+        }
+
+    response = requests.request("GET", url, headers=headers, params=querystring)
+
+    return response.text
