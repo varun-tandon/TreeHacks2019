186e66e4f3b307c998d0c52abe89e5fab1c96911
rlama531 <roberto.lama18@gmail.com>
2019-02-16 03:33:11 -0800

    Added send_sms_to_user to main py

--- main.py
+++ main.py
@@ -92,3 +92,19 @@ def get_governor_access():
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     return response.text
+
+@app.route('/send_sms_to_user', methods=['GET'])
+def send_sms_to_user():
+    url = "https://api.authy.com/protected/json/phones/verification/start"
+    content = request.get_json()
+    phone_number = content["phone_number"]
+    payload = "api_key=tzsvpYNDQSEsQqFnYT5sNeCWNLXs7Qum&via=sms&phone_number=" + phone_number + "&country_code=1&undefined="
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'cache-control': "no-cache",
+        'Postman-Token': "b6895b26-2acb-40bd-9e35-60fc2d1634a8"
+        }
+
+    response = requests.request("POST", url, data=payload, headers=headers)
+
+    print(response.text)
