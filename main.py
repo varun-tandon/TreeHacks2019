2f6c08c2b211685c4a0de7c47f64c03b54b7f051
varun-tandon <vrntandon@gmail.com>
2019-02-16 01:58:45 -0800

    Add os path code

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, url_for, request
 import json
-import os
+import os, sys
 import random
+import hashlib, uuid
+
+from os.path import dirname
+sys.path.append(dirname(__file__))
+
 from .src.entities.entity import Session, engine, Base
 from .src.entities.user import User
 
@@ -19,24 +24,6 @@ def homepage():
 @app.route('/create_user', methods=['POST'])
 def create_user():
     request_json = request.get_json()
-    new_user = User(request_json['email'], request_json['password'])
     session.add(new_user)
     session.commit()
     return "completed"
-
-
-@app.route('/access_rep_info', methods=['GET'])
-def get_rep_access():
-    url = "https://q4ktfaysw3.execute-api.us-east-1.amazonaws.com/treehacks/legislators"
-    content = request.get_json()
-
-    querystring = {"address": content['zipcode']}
-
-    headers = {
-        'x-api-key': "2e1uvo7yeX50ZGHvctPxi8ZWubhggyOydIWvOa5c",
-        'Content-Type': "application/x-www-form-urlencoded",
-        }
-
-    response = requests.request("GET", url, headers=headers, params=querystring)
-
-    return response.text
