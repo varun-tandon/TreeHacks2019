ad4b9d4d3a1eae867d3565cdc2ecb9a9506bd31f
varun-tandon <vrntandon@gmail.com>
2019-02-16 02:02:37 -0800

    Remove dysfunctional import fix

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import os, sys
 import random
 import hashlib, uuid
 
-from os.path import dirname
-sys.path.append(dirname(__file__))
-
 from .src.entities.entity import Session, engine, Base
 from .src.entities.user import User
 
@@ -24,6 +21,8 @@ def homepage():
 @app.route('/create_user', methods=['POST'])
 def create_user():
     request_json = request.get_json()
+    hashed_password = hashlib.sha512(request_json['password'].encode('utf-8')).hexdigest()
+    new_user = User(request_json['email'], hashed_password)
     session.add(new_user)
     session.commit()
     return "completed"
